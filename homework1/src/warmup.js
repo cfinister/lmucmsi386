30d3220d26cfd346f82ad375b7e5f774a4a93e02
Cherrell Finister <cfiniste@lion.lmu.edu>
2017-09-07 18:09:17 -0700

    updated the code

--- homework1/src/warmup.js
+++ homework1/src/warmup.js
@@ -1,1 +1,10 @@
 // cherrell homework goes here
+
+function change(amount) {
+  let result =[]
+  [25, 10, 5, 1].forEach(coin => {
+    amount = Math.floor(amount / coin); 
+    result.push(amount % coin); 
+  }); 
+  return result;
+}; 
