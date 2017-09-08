560ef1e5d85790866ce02304231396658ac90af7
Cherrell Finister <cfiniste@lion.lmu.edu>
2017-09-07 18:16:38 -0700

    updated

--- homework1/src/warmup.js
+++ homework1/src/warmup.js
@@ -1,6 +1,5 @@
 // cherrell homework goes here
 
-<<<<<<< HEAD
 function change(amount) {
   let result =[]
   [25, 10, 5, 1].forEach(coin => {
@@ -13,4 +12,3 @@ function change(amount) {
 exports.change = () => { 
   return [0, 0, 0, 0];
 }
->>>>>>> 04376ec270b523fc992e0d67dc0505ff9247a796
