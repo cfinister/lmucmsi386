c730f73a0a4cc595ce2147d65370b79578915843
Cherrell Finister <cfiniste@lion.lmu.edu>
2017-09-08 20:17:08 -0700

    testing the warmup

--- homework1/warmup.js
+++ homework1/warmup.js
@@ -1,1 +1,34 @@
 // cherrell homework goes here
+
+function change(amount) {
+if (amount < 0) {
+throw new RangeError('amount cannot be negative');
+}
+const result =[];
+let remaining = amount;
+[25, 10, 5, 1].forEach((coin) => {
+amount = Math.floor(remaining / coin);
+remaining %= coin;
+});
+return result;
+}
+
+module.exports = {
+change,
+};
+
+/*function stripQuotes(text) {
+  var text = ("");
+  if (text!= '') {
+    var str1 = text.replace(/\,/g,"");
+  }
+}; */
+
+function scramble() {
+  var scramble = '';
+      word = word.split('');
+      while (word.length > 0) {
+        scramble +=  word.splice(word.length * Math.random() << 0, 1);
+      }
+      return scramble;
+};
