58a04170a5063a617512d447471a44bcdd9dface
Cherrell Finister <cfiniste@lion.lmu.edu>
2017-09-12 19:48:36 -0700

    much pretty very linted

--- homework1/src/warmup.js
+++ homework1/src/warmup.js
@@ -1,104 +1,103 @@
 // Homework 1 Warmup to javascript - now due on Wednesday
-//Sept. 15th @ midnight on git hub - do not forget to push!
+// Sept. 15th @ midnight on git hub - do not forget to push!
 
 function change(amount) {
-    if (amount < 0) {
-        throw new RangeError('amount cannot be negative');
-    }
-    const zero = [0, 0, 0, 0];
-    if (amount == 0) {
-        return zero;
-    }
+  if (amount < 0) {
+    throw new RangeError('amount cannot be negative');
+  }
+  const zero = [0, 0, 0, 0];
+  if (amount === 0) {
+    return zero;
+  }
 
-    let coin = [25, 10, 5, 1];
-    var remaining = amount;
-    for (i = 0; i < coin.length; i++) {
-        var temp = Math.floor(remaining / coin[i]); //using temp to help w/ remaining math amounts
-        remaining = remaining - (temp * coin[i]);
-        coin[i] = temp;
-    }
+  const coin = [25, 10, 5, 1];
+  let remaining = amount;
+  for (let i = 0; i < coin.length; i += 1) {
+    const temp = Math.floor(remaining / coin[i]); // using temp to help w/ remaining math amounts
+    remaining -= (temp * coin[i]);
+    coin[i] = temp;
+  }
 
-    return coin;
+  return coin;
 }
 
 function stripQuotes(text) {
-    //the gobal flag is used for the function to solve this gobally
-    let string = text;
-    string = string.replace(/"/g, ''); //thanks to John & Jordan for helping
-    string = string.replace(/\\/g, ''); //helped w/ getting to figure out how to use a global flag
-    string = string.replace(/'/g, '');
+  // the gobal flag is used for the function to solve this gobally
+  let string = text;
+  string = string.replace(/"/g, ''); // thanks to John & Jordan for helping
+  string = string.replace(/\\/g, ''); // helped w/ getting to figure out how to use a global flag
+  string = string.replace(/'/g, '');
 
-    return string;
+  return string;
 }
 
 function scramble(word) {
-    let scramble = '';
-    word = word.split('');
-    while (word.length > 0) {
-        scramble += word.splice(word.length * Math.random() << 0, 1);
-    }
-    return scramble;
-};
+  let scrambled = '';
+  const letters = word.split('');
+  while (letters.length > 0) {
+    scrambled += letters.splice(Math.floor(letters.length * Math.random()), 1);
+  }
+  return scrambled;
+}
 
 function powers(base, limit, callBack) {
-  for (let i = 0; base**i <= limit; i++) {
+  for (let i = 0; base ** i <= limit; i += 1) {
     callBack(base ** i);
   }
 }
 
 function* powersGenerator() {
-    //code goes here
-};
+  // code goes here
+}
 
 function say() {
-    let words = [];
-    const nextWord = word;
-    if (nextWord == null) {
-        return '';
-    } else {
-        words.push(nextWord);
-        return words.join(" ");
-    }
-};
+  const words = [];
+  const nextWord = word;
+  if (nextWord == null) {
+    return '';
+  }
+  words.push(nextWord);
+  return words.join(' ');
+}
 
 function interleave() {
-    //code goes here
-};
+// code goes here
+}
 
-function cylinder(radius, height, surfaceArea, volume, widen, stretch, toString) {
-    let c = cylinder;
-    let cylinder = {};
-    c.surfaceArea() = 0;
-    c.volume() = 0;
-    c.radius = 0;
-    c.height = 0;
-    c.widen() = 0;
-    c.stretch() = 0;
-    c.toString() = 0;
-};
+function cylinder(radius, height, surfaceArea, volume, widen, stretch) {
+  // let c = cylinder;
+  // let cylinder = {};
+  // //c.surfaceArea() = 0;
+  // c.volume() = 0;
+  // c.radius = 0;
+  // c.height = 0;
+  // c.widen() = 0;
+  // c.stretch() = 0;
+  // c.toString() = 0;
+}
 
 function encrypt() {
-    //this one goes with function decrypt
-};
+  // this one goes with function decrypt
+}
 
 function decrypt() {
-    //this one goes with function encrypt
-};
+  // this one goes with function encrypt
+}
 
 function randomName() {
-    //code goes here
-};
+  // code goes here
+}
 
 module.exports = {
-    change,
-    stripQuotes,
-    scramble,
-    powers,
-    powersGenerator,
-    say,
-    interleave,
-    cylinder,
-    encrypt,
-    decrypt,
-    randomName,
+  change,
+  stripQuotes,
+  scramble,
+  powers,
+  powersGenerator,
+  say,
+  interleave,
+  cylinder,
+  encrypt,
+  decrypt,
+  randomName,
 };
