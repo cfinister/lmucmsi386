f288872c1484d9f119e5aa9e341be1181fab5bbc
Cherrell Finister <cfiniste@lion.lmu.edu>
2017-09-28 20:20:11 -0700

    updated homework and added linter

--- homework2/warmup.py
+++ homework2/warmup.py
@@ -1,4 +1,36 @@
 def change(amount):
-    pass
+    # return (0, 0, 0, 0)
+    coins = [25, 10, 5, 1]
+    result = {}
+    for coin_value, coin_name in coins:
+        if amount >= coin_value:
+            number_coin, amount = divmod(amount, coin_value)
+            result[coin_name] = number_coin
+    return result
+
 def strip_quotes(s):
+    pass
+
+def scramble(s):
+     pass
+
+def say(new_words):
+     pass
+
+def triples(t):
+    pass
+
+def powers(base, limit):
+    pass
+
+def interleave(words, *more_words):
+    pass
+
+def Cylinder(radius, height):
+    pass
+
+def make_crypto_functions(key, iv):
+    pass
+
+def random_name(r):
     pass
