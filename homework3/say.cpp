ceb88483e39d4ace0a738f752dd6f8e32deeeddc
Cherrell Finister <cfiniste@lion.lmu.edu>
2017-11-14 11:05:04 -0800

    worked on say

--- homework3/say.cpp
+++ homework3/say.cpp
@@ -5,3 +5,6 @@ int main() {
   assert(say("hello")() == "hello");
   assert(say("how")("are")("you")() == "How are you");
 }
+
+#define O(P)operator()(P){return{P+a};}int
+struct F{F O(int(m))O()a;}f;
